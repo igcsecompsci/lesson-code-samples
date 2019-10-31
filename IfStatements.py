c58296c450e388c0c592d3ea36ac699a10296d28
Robert Griffiths <34591246+robertgriffiths@users.noreply.github.com>
2019-10-31 11:40:16 -0500

    Create IfStatements.py

---
+++ IfStatements.py
@@ -0,0 +1,24 @@
+# Use of if statements
+
+# Check if a number is less than another number
+if 20 > 15:
+  print("20 is greater than 15.")
+else:
+  print("20 is not greater than 15.")
+  
+  
+# Check if a number is the same as another number
+if 20 == 20:
+  print("Both numbers are the same.")
+else:
+  print("Both numbers are different.")
+
+
+# Checking a person's age using a variable
+age = 16
+
+if age >= 16:
+  print("You are old enough to watch the movie.")
+else:
+  print("You are too young to watch the movie.")
+  
