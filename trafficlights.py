e48e9eed4e0c2ad2779e2bf3310e2629d77d066a
Robert Griffiths <34591246+robertgriffiths@users.noreply.github.com>
2020-09-09 19:22:25 -0500

    Create trafficlights.py

---
+++ trafficlights.py
@@ -0,0 +1,10 @@
+# A program that decides what to do depending on the colour of the light
+
+light = input("What colour is the light?: ")
+
+if light == "Red":
+    print("The light is {}. You should stop.".format(light))
+elif light == "Orange":
+    print("The light is {}. You should wait.".format(light))
+else:
+    print("The light is {}. You can go.".format(light))
