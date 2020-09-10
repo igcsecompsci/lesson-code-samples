1b2a29562c6df88507fa2e4ae33fdd12f66323d5
Robert Griffiths <34591246+robertgriffiths@users.noreply.github.com>
2020-09-09 19:46:04 -0500

    Create EscapeCharacters.py

---
+++ EscapeCharacters.py
@@ -0,0 +1,9 @@
+# Use of \t (tabs)
+print("Name\t\tCity\t\tTelephone")
+print("Bob\t\tBogota\t\t0123456789")
+
+# Use of \n (newline)
+print("\nMon\nTue\nWed\nThu\nFri")
+
+# Use of \r (carriage return)
+print("\rMon\rTue\rWed\rThu\rFri")
