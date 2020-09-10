fc10b0678c970de424d6c604e579c0229515c153
Robert Griffiths <34591246+robertgriffiths@users.noreply.github.com>
2020-09-09 19:32:24 -0500

    Update IfStatements.py

--- IfStatements.py
+++ IfStatements.py
@@ -22,3 +22,15 @@ if age >= 16:
 else:
   print("You are too young to watch the movie.")
   
+  
+# A program that simulates traffic lights using if-elif-else
+
+light = input("What colour is the light?: ")
+
+if light == "Red":
+    print("The light is {}. You should stop.".format(light))
+elif light == "Orange":
+    print("The light is {}. You should wait.".format(light))
+else:
+    print("The light is {}. You can go.".format(light))
+  
